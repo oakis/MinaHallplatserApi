589037ee1e40d10675d328398bfc2a6cbc5db25e
oakis <oakismen@gmail.com>
2017-12-03 14:04:15 +0100

    add helper

---
+++ MinaHallplatserApi/Helpers/HttpRequestHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MinaHallplatserApi.Helpers
+{
+    public static class HttpRequestHelper
+    {
+        public static void ThrowIfNotOk(HttpResponseMessage response)
+        {
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+                else
+                {
+                    throw new HttpRequestException();
+                }
+            }
+        }
+    }
+}
---
+++ MinaHallplatserApi/Helpers/JsonConverterHelper.cs
@@ -0,0 +1,35 @@
+﻿using MinaHallplatserApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinaHallplatserApi.Helpers
+{
+    public class JsonConverterHelper : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartArray)
+            {
+                return serializer.Deserialize<List<StopLocation>>(reader);
+            }
+            else
+            {
+                StopLocation media = serializer.Deserialize<StopLocation>(reader);
+                return new List<StopLocation>(new[] { media });
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
